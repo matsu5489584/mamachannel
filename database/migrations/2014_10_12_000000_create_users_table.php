7e5ebadcf725fa09a93508722ee5c7f6806f8ee8
matsushita <matsushita@matsushitakazuminoMacBook-ea.local>
2018-12-19 06:42:21 +0900

    カラム追加

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->string('summary') 
             $table->increments('id');
             $table->string('name');
+            $table->string('birthday')
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
