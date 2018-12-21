cd0971ebeb390befdf84a78c5c709ea95b62790a
matsushita <matsushita@matsushitakazuminoMacBook-ea.local>
2018-12-21 23:28:50 +0900

    Acton

--- app/Http/Controllers/Admin/TopicsController.php
+++ app/Http/Controllers/Admin/TopicsController.php
@@ -7,5 +7,9 @@ use App\Http\Controllers\Controller;
 
 class TopicsController extends Controller
 {
-    //
+    // 以下を追記
+  public function add()
+  {
+      return view('admin.topics.create');
+  }
 }
