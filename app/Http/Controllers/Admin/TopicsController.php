cb3a80cd189f8b4100628393cc5ae676407169e3
matsushita <matsushita@matsushitakazuminoMacBook-ea.local>
2018-12-23 23:52:03 +0900

    138完了

--- app/Http/Controllers/Admin/TopicsController.php
+++ app/Http/Controllers/Admin/TopicsController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+
+use App\Topics;
 
 class TopicsController extends Controller
 {
@@ -15,7 +16,30 @@ class TopicsController extends Controller
   // 以下を追記
     public function create(Request $request)
     {
-        // admin/topics/createにリダイレクトする
-        return redirect('admin/topics/create');
+            // 以下を追記
+            // Varidationを行う
+            $this->validate($request, Topics::$rules);
+
+            $news = new Topics;
+            $form = $request->all();
+
+            // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+            if (isset($form['image'])) {
+              $path = $request->file('image')->store('public/image');
+              $news->image_path = basename($path);
+            } else {
+                $news->image_path = null;
+            }
+
+            // フォームから送信されてきた_tokenを削除する
+            unset($form['_token']);
+            // フォームから送信されてきたimageを削除する
+            unset($form['image']);
+
+            // データベースに保存する
+            $news->fill($form);
+            $news->save();
+
+            return redirect('admin/topics/create');
     }
   }
